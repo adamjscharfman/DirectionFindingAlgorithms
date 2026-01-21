27b07039cadc9cf52b5db878b1e59d72aa6e0377
adamjscharfman <adamjscharfman@gmail.com>
2026-01-21 14:32:59 -0500

    Added function for computing a ura, and computing the ura subarray indices

--- lib/manifold/array_manifold.py
+++ lib/manifold/array_manifold.py
@@ -2,7 +2,7 @@ import numpy as np
 
 speed_of_light = 299792458
 
-def ideal_manifold_vectorized(freq_list, phi_list, theta_list, rx_coords):
+def ideal_manifold_vectorized(freq_list, phi_list, theta_list, rx_coords)->np.ndarray:
     """
     Vectorized computation of the array manifold.
 
@@ -119,7 +119,7 @@ def ideal_manifold_linear_array_loop(freq_list:np.array,theta_list:np.array,rx_c
 
     return A
 
-def antenna_pattern_estimate_loop(freq_list:np.array,phi_list:np.array,theta_list:np.array,rx_coords:np.ndarray):
+def antenna_pattern_estimate_loop(freq_list:np.array,phi_list:np.array,theta_list:np.array,rx_coords:np.ndarray)->tuple[np.ndarray,np.ndarray]:
     '''
     Inputs:
     freq_list - (K) array of frequencies
@@ -162,3 +162,49 @@ def antenna_pattern_estimate_loop(freq_list:np.array,phi_list:np.array,theta_lis
                 antenna_pattern[f_ind,phi_ind,theta_ind] = np.dot(A_0[f_ind].conj(),A[f_ind,phi_ind,theta_ind,:])
 
     return A,antenna_pattern
+
+def compute_ura_coords(Nx:int,Ny:int,lam:float) -> np.array:
+    '''
+    Computes zero centered uniform rectangular array coordinates with lam/2 spacing, note with even sized arrays, there is no element at 0
+    Inputs:
+    Nx - Number of x antenna elements
+    Ny - Number of y antenna elements
+    lam - Wavelength
+    Returns:
+    rx_coords - (L x 3) array of antenna positions in x,y,z
+    '''
+    num_ch = Nx*Ny
+    rx_coords = np.empty((num_ch,3))
+    center_correction_x = 0
+    center_correction_y = 0
+    if (Nx%2==0):
+        center_correction_x = 0.5
+    if (Ny%2==0):
+        center_correction_y = 0.5
+    for a in range(Nx): 
+        for b in range(Ny):
+            rx_coords[a*Ny+b,:] = np.array([0,a-Nx//2+center_correction_x,b-Ny//2+center_correction_y])*lam/2
+
+    return rx_coords
+
+def ura_subarray_indices(Nx:int, Ny:int, Sx:int, Sy:int)->list:
+    """
+    Compute the subarray indices for an arbitrary uniform rectangular array
+    Inputs:
+    Nx: Number of antennas in x dimension
+    Ny: Number of antennas in y dimension
+    Sx: Number of subantennas in x dimension
+    Sy: Number of subantennas in y dimension
+    """
+    subarrays = []
+
+    for j0 in range(Ny - Sy + 1):
+        for i0 in range(Nx - Sx + 1):
+            indices = []
+            for j in range(Sy):
+                for i in range(Sx):
+                    idx = (i0 + i) + (j0 + j) * Nx
+                    indices.append(idx)
+            subarrays.append(indices)
+
+    return subarrays
